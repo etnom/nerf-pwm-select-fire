104696e2698c3672932b54d890ed4c80c4f65921
Monte <montgomery.okdaily@gmail.com>
2017-10-20 18:46:45 +0000

    macro and pinode trigger pin vals

--- nerf-pwm-select-fire.ino
+++ nerf-pwm-select-fire.ino
@@ -3,11 +3,18 @@
 #define JOYSTICK_VERT 1
 #define JOYSTICK_HORIZ 2
 
+#define REV_TRIGGER 1
+#define PUSHER_TRIGGER 0
+
 #define FLYWHEELS_PWM 3
 #define PUSHER_PWM 11
 
+byte fireMode = 0;
+
+
 void setup () {
-    
+    pinMode(REV_TRIGGER, INPUT);
+    pinMode(PUSHER_TRIGGER, INPUT);
 }
 
 void loop () {
